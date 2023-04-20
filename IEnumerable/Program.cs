b828f73fbb7f31a2d943349ee20ac81ce84a366e
Wizzadrix <666_havoc@gmx.de>
2023-04-20 11:50:27 +0200

    Projektdateien hinzufügen.

---
+++ IEnumerable/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+
+namespace IEnumerable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PhoneBook MyPhoneBook = new PhoneBook();
+
+            foreach (Contact contact in MyPhoneBook)
+            {
+                contact.Call();
+            }
+
+            Console.ReadKey();
+        }
+
+        public class Contact
+        {
+            public string Name { get; set; }
+            public string PhoneNumber { get; set; }
+
+            public Contact(string name, string phoneNumber)
+            {
+                Name = name;
+                PhoneNumber = phoneNumber;
+            }
+
+            public void Call()
+            {
+                Console.WriteLine("Calling to {0}. Phone number is {1}", Name, PhoneNumber);
+
+            }
+        }
+
+        public class PhoneBook : IEnumerable<Contact>
+        {
+
+            List<Contact> contacts = new List<Contact>
+            {
+                new Contact("Andre", "435797087"),
+                new Contact("Lisa", "435677087"),
+                new Contact("Dine", "3457697087"),
+                new Contact("Sofi", "4367697087")
+            };
+
+            public IEnumerator GetEnumerator()
+            {
+                return contacts.GetEnumerator();
+            }
+
+            IEnumerator<Contact> IEnumerable<Contact>.GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+
+        }
+
+
+    }
+}
